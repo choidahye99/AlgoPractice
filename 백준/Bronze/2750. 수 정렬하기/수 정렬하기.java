d1db3f2f2765060efad9e67b6fe0f0f4ddb7beb3
choidahye99 <109265266+choidahye99@users.noreply.github.com>
2022-08-12 09:43:23 +0900

    [Bronze II] Title: 수 정렬하기, Time: 240 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,56 +1,26 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] arr = new int[N];
-		
-		for(int i=0; i<N; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int[] arr2 = Arrays.copyOfRange(mergeSort(arr), 0, arr.length);
-		for(int i=0; i<N; i++) {
-			System.out.println(arr2[i]);
-		}
-	}
-	
-static int[] mergeSort(int[] arr) {
-		
-		if(arr.length == 1) {
-			return arr;
-		}
-		int[] arr1 = mergeSort(Arrays.copyOf(arr, arr.length/2));
-		int[] arr2 = mergeSort(Arrays.copyOfRange(arr,arr.length/2 , arr.length));
-		return merge(arr1,arr2);		
-			
-	}
-	static int[] merge(int[] arr1, int[] arr2) {
-		int[] mergedArr = new int[arr1.length+arr2.length];
-		
-		int i=0,i1=0,i2=0;
-		while(i<arr1.length+arr2.length) {
-			if(i1 == arr1.length) {
-				mergedArr[i] = arr2[i2];
-				i2++;
-			}else if(i2 == arr2.length) {
-				mergedArr[i] = arr1[i1];
-				i1++;
-			}else if(arr1[i1] <arr2[i2]) {
-				mergedArr[i] = arr1[i1];
-				i1++;
-			}else {
-				mergedArr[i] = arr2[i2];
-				i2++;
-			}
-			
-			i++;
-		}
-		return mergedArr;
-	}
-
+import java.util.Scanner ;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i =0; i<N; i++){
+            arr[i] = sc.nextInt(); 
+        }
+        sort(arr) ;
+        for (int a : arr){
+            System.out.println(a+"");
+        }
+    }
+    static void sort(int[] arr){
+        for(int i = 1 ; i < arr.length ; i++){
+            for(int j = i ; j > 0 ; j--){
+                if(arr[j]<arr[j-1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = tmp;
+                }
+            }
+        }
+}
 }
