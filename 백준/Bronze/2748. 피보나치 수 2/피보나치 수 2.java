19a8143bb228ccd8276519ce780215eb1636a335
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-08-19 17:25:08 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,47 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-	// 시간초과가 떠버렸다..
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		
-//		int n = sc.nextInt();
-//		
-//		System.out.println(fibo(n));
-//	}
-//	
-//	public static int fibo(int n) {
-//		if(n==0) return 0;
-//		if(n==1) return 1;
-//		
-//		return fibo(n-1)+fibo(n-2);
-//	}
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt();
-
-		System.out.println(fibo(n));
-	}
-
-	public static long fibo(int n) {
-		long[] answer = new long[n + 1]; // 피보나치를 담을 배열. n번까지의 인덱스를 가짐
-
-		for (int i = 0; i <= n; i++) {
-
-			if (i == 0) {
-				answer[i] = 0;
-			} else if (i == 1) {
-				answer[i] = 1;
-			}
-			else {
-				long sum = answer[i - 2] + answer[i - 1]; // 피보나치
-				answer[i] = sum ;
-			}
+		
+		int N = sc.nextInt();
+		
+		long a = 0;
+		long b = 1;
+		
+		for(int i=0; i<N-1; i++) {
+			long temp = a;
+			a = b;
+			b = temp+a;
 		}
-
-		return answer[n];
+		
+		System.out.println(b);
 	}
 
 }
