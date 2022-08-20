585ae74f0391b5938dd70ebbf90c97e620718bf8
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-08-21 00:19:10 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 12972 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,34 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		int target = sc.nextInt();
+		int cardAmt = sc.nextInt(); // 카드 개수
+		int M = sc.nextInt(); // 딜러가 외친 숫자
+		int[] cardArr = new int[cardAmt];
+
+		for (int i = 0; i < cardAmt; i++) {
+			cardArr[i] = sc.nextInt();
+		} // 가지고 있는 카드
+
+
+		int result = 0; //결과값
 		
-		int[] arr= new int[N];
-		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
-		
-		}
-		
-		int max = 0;
-		for(int i=0; i<arr.length; i++) {
-			for(int j=i+1; j<arr.length; j++) {
-				for(int k = j+1; k<arr.length; k++) {
-					int testnum = arr[i]+arr[j]+arr[k];
-					if(testnum>max && testnum<=target) {
-						max = testnum;
+		for (int i = 0; i < cardAmt - 2; i++) { // 마지막 두 장은 의미X
+			for (int j = i + 1; j < cardAmt - 1; j++) {
+				for (int k = j + 1; k < cardAmt; k++) {
+					
+					int add = cardArr[i] + cardArr[j] + cardArr[k]; // 카드 세 개 더한 총합 모두 구하기
+					
+					if(add == M) {
+						System.out.println(add);
+						return;
+					} 
+					
+					if(result < add && add<M) {
+						result = add;
 					}
 				}
+
 			}
+
 		}
 		
-		System.out.println(max);
+		System.out.println(result);
+
 	}
 
 }
