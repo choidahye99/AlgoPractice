ff6efa119cef02c44f07a7ed8b768edf7af90447
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-08-23 17:38:16 +0900

    [Gold II] Title: 트리의 지름, Time: 832 ms, Memory: 113620 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -3,13 +3,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+
 import java.util.StringTokenizer;
 
 public class Main {
-	
+	static int sum = 0;
+	static int max = 0;
+	static int idx = 0;
 	static ArrayList[] arr;
 	public static void main(String[] args) throws  IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,62 +39,35 @@ public class Main {
 			
 		}
 		
-		Queue<Integer> q = new LinkedList<Integer>();
-		int[] sum = new int[N+1];
 		
-		q.add(1);
-		boolean isVisited[] = new boolean[N+1];
+		// 이제 최대값 찾기 
+		boolean[] isVisited = new boolean[N+1];
 		isVisited[1] = true;
-		int max = 0;
-		int idx = 0;
-		while(!q.isEmpty()) {
-			for(int i=0; i<arr[q.peek()].size(); i++) {
-				
-				int newNode = (int) ((ArrayList) arr[q.peek()].get(i)).get(0);
-				int newDis = (int) ((ArrayList) arr[q.peek()].get(i)).get(1);
-				if(!isVisited[newNode]) {
-					q.add(newNode);
-					sum[newNode] = sum[q.peek()]+newDis;
-					if(max <sum[newNode]) {
-						max = sum[newNode];
-						idx = newNode;
-					}
-					isVisited[newNode] = true;
-				}
-				
-			}
-			q.poll();
-		}
+		dfs(1,isVisited);
 		
-		
-		
-		// idx 기준으로 다시 한번 
-		q.clear();
-		sum = new int[N+1];
-		q.add(idx);
 		isVisited = new boolean[N+1];
+		max = 0;
 		isVisited[idx] = true;
-		
-		while(!q.isEmpty()) {
-			for(int i=0; i<arr[q.peek()].size(); i++) {
-				
-				int newNode = (int) ((ArrayList) arr[q.peek()].get(i)).get(0);
-				int newDis = (int) ((ArrayList) arr[q.peek()].get(i)).get(1);
-				if(!isVisited[newNode]) {
-					q.add(newNode);
-					sum[newNode] = sum[q.peek()]+newDis;
-					if(max <sum[newNode]) {
-						max = sum[newNode];
-						idx = newNode;
-					}
-					isVisited[newNode] = true;
-				}
-				
-			}
-			q.poll();
-		}
+		dfs(idx , isVisited);
 		System.out.println(max);
 	}
 	
-	
+	static void dfs(int node,boolean[] isVisited) {
+		for(int i=0; i<arr[node].size(); i++) {
+			int newNode = (int) ((ArrayList) arr[node].get(i)).get(0);
+			int newDis = (int) ((ArrayList) arr[node].get(i)).get(1);
+			if(!isVisited[newNode]) {
+				isVisited[newNode] = true;
+				sum += newDis;
+				if(max<sum) {
+					max = sum;
+					idx = newNode;
+				}
+				dfs(newNode, isVisited);
+				sum-= newDis;
+				isVisited[newNode] = false;
+				
+			}
+		}
+	}
 }
