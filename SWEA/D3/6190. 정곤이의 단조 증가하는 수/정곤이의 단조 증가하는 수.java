b67f30327d50c1668402df7e7653ca26694af7ae
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-08-26 11:58:51 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 889 ms, Memory: 159,220 KB -BaekjoonHub

---
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt(); //테스트케이스
+		
+		for(int tc=1; tc<=T; tc++) {
+			
+			int N = sc.nextInt(); //입력받을 숫자 크기
+			int[] num = new int[N];
+			
+			for(int i=0; i<N; i++) {
+				num[i] = sc.nextInt();
+			}
+			
+			int max = 0;
+			
+			int i=0;
+			int j = i+1;
+
+			while(i<N-1) {
+				String mNum = String.valueOf(num[i]*num[j]);
+				
+				int cnt=0;
+				for(int k=0; k<mNum.length()-1; k++) {
+					char ch = mNum.charAt(k);
+					char ch2 = mNum.charAt(k+1);
+					if(ch-'0'<=ch2-'0') {
+						cnt++;
+					} else break;
+				}
+				
+				if(cnt==mNum.length()-1) max=Math.max(max, Integer.parseInt(mNum));
+				
+				j++;
+				if(j>N-1) {
+					i++;
+					j = i+1;
+				}
+			}
+			
+            if(max==0) max = -1;
+            
+			System.out.println("#"+tc+" "+max);
+			
+			
+			
+		}//testCase
+	} //main
+
+}
