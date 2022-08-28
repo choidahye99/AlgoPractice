3d4cb8bbbf1b4e0e147d493bc5bd9c7654c741eb
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-08-28 22:39:43 +0900

    [Silver I] Title: 직사각형, Time: 112 ms, Memory: 12992 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.java
+++ 백준/Silver/2527. 직사각형/직사각형.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < 4; i++) {
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int p1 = sc.nextInt();
-			int q1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
-			int p2 = sc.nextInt();
-			int q2 = sc.nextInt();
-			
-			if (p1 < x2 || q1 < y2 || p2 < x1 || q2 < y1) {
-				System.out.println("d");
-			}
-			else if ((x1 == p2 && y1 == q2) || (x1 == p2 && q1 == y2) || (p1 == x2 && q1 == y2) || (p1 == x2 && y1 == q2)) {
-				System.out.println("c");
-			}
-			else if (p1 == x2 || q1 == y2|| p2 == x1 || y1 == q2){
-				System.out.println("b");
-			}
-			else {
-				System.out.println("a");
-			}
-		}
-	}
+		
 
+		for (int t = 0; t < 4; t++) {
+			int j1 = sc.nextInt();
+			int i1 = sc.nextInt();
+			int j2 = sc.nextInt();
+			int i2 = sc.nextInt();
+
+			int j3 = sc.nextInt();
+			int i3 = sc.nextInt();
+			int j4 = sc.nextInt();
+			int i4 = sc.nextInt();
+			
+			//겹치지 않을 경우
+			if(j2<j3||j4<j1||i2<i3||i4<i1) System.out.println("d");
+			
+			//점
+			else if((j2==j3&&i2==i3)||(j1==j4&&i2==i3) ||(j2==j3&&i1==i4)||(j1==j4&&i1==i4)) System.out.println("c");
+			
+			//선분
+			else if(j3==j2||j4==j1||i2==i3||i1==i4) System.out.println("b");
+			
+			//나머지 다 직사각형
+			else System.out.println("a");
+		}
+
+	}
 }
