bc0059be10d5fd4594675a37f4bf32e200a1b332
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-09-02 23:43:28 +0900

    [Silver IV] Title: 큐, Time: 336 ms, Memory: 29588 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,54 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new LinkedList<>(); 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int N = sc.nextInt();
 		
-		int lastout = 0;
-		for(int i=0 ; i<N; i++) {
-			String command = br.readLine();
-			st = new StringTokenizer(command," ");
-			switch(st.nextToken()) {
+		StringBuilder sb = new StringBuilder();
+		
+		//Queue 선언
+		Queue<Integer> que = new LinkedList<>();
+		
+		int last = 0;
+		
+		for(int i=0; i<N; i++) { //명령어 개수만큼 돕니다
+			
+			String commend = sc.next(); //명령어
+			int r = 0;
+			if(commend.equals("push")) {
+				int num = sc.nextInt();
+				r=num;
+			}
+			
+			switch(commend) {
 			case "push":
-				int num = Integer.parseInt(st.nextToken());
-				q.add(num);
-				lastout = num; 
-				break;
-			case "front":
-				if(q.isEmpty()) System.out.println(-1);
-				else System.out.println(q.peek());
-				break;
-			case "back":
-				if(q.isEmpty()) System.out.println(-1);
-				else System.out.println(lastout);
-				break;
-			case "size":
-				System.out.println(q.size());
-				break;
-			case "empty":
-				if(q.isEmpty()) System.out.println(1);
-				else System.out.println(0);
-				break;
-			case "pop":
-				if(q.isEmpty()) System.out.println(-1);
-				else System.out.println(q.poll());
-				break;
-			default:
+				last = r;
+				que.offer(last);
 				break;
 				
+			case "pop" :
+				if(que.isEmpty()) {
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(String.valueOf(que.poll())).append("\n");
+				}
+				break;
+			case "size":
+				sb.append(que.size()).append("\n");
+				break;
+			case "empty":
+				if(que.isEmpty()) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+				break;
+			case "front":
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(que.peek()).append("\n");
+				break;
+			case "back":
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(last).append("\n");
+				break;
 			}
+			
 		}
+		
+		System.out.println(sb);
+		
 	}
 
 }
