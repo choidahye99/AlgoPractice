ef5deb54d1329f6cc4d93b1bf0894db50b7c681b
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-09-07 23:17:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 156 ms, Memory: 16584 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,30 +1,54 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	static int N;
+	static int[] num;
+	static ArrayList<Integer> list;
+
+	public static int binarySearch(int i) {
+		int start = 0;
+		int end = list.size() - 1;
+
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (list.get(mid) >= i) {
+				end = mid;
+			} else {
+				start = mid + 1;
+			}
+
+		}
+
+		return end;
+
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt(); //수열의 크기
-		
-		int[] num = new int[N+1];
-		int[] dp = new int[N+1];
-		
-		for(int i=1; i<=N; i++) {
+
+		N = sc.nextInt();
+
+		num = new int[N+1];
+		list = new ArrayList<>();
+
+		for (int i = 1; i <= N; i++) {
 			num[i] = sc.nextInt();
 		}
-		
-		int ans = 0;
-		for(int i=1; i<=N; i++) {
-			dp[i] = 1;
-			for(int j=1; j<i; j++) {
-				if(num[j]<num[i]) {
-					dp[i] = Math.max(dp[j]+1,dp[i]);
-				}
+
+		list.add(0);
+
+		for (int i = 1; i <= N; i++) {
+			if(num[i]>list.get(list.size()-1)) {
+				list.add(num[i]);
+			} else {
+				
+				int idx = binarySearch(num[i]);
+				list.set(idx, num[i]);
 			}
-			
-			ans = Math.max(ans, dp[i]);
 		}
 		
-		System.out.println(ans);
+		System.out.println(list.size()-1);
 	}
+
 }
