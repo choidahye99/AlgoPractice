2bfc31cc3ba661806eeb26eb5cc7e6f51f8b6221
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2022-10-04 17:42:43 +0900

    [Gold IV] Title: 운동, Time: 540 ms, Memory: 58268 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -1,62 +1,64 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	static final int INF = 987654321;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int V = sc.nextInt(); //마을의 개수
-		int E = sc.nextInt(); //도로의 개수
-		
-		int[][] dist = new int[V+1][V+1];
+	static int[][] town;
+	static int V, E, result;
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		//참고
-		for(int i=1; i<=V; i++) {
-			for(int j=1; j<=V; j++) {
-				if(i != j)
-					dist[i][j] = INF;
-			}
+		V = Integer.parseInt(st.nextToken()); // 마을 개수
+		E = Integer.parseInt(st.nextToken()); // 간선
+
+		result = INF;
+		town = new int[V + 1][V + 1];
+
+		// 초기 설정
+		for (int i = 1; i <= V; i++) {
+			Arrays.fill(town[i], INF);
 		}
-		
-		for(int i=0; i<E; i++) {
-			int st = sc.nextInt(); //시작마을
-			int ed = sc.nextInt(); //도착마을
-			int cost = sc.nextInt(); //비용
-			
-			 dist[st][ed] = cost; //일방통행입니당
+
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int len = Integer.parseInt(st.nextToken());
+
+			// 중복되는 간선이 없다 했으므로
+			town[start][end] = len;
 		}
-		
-		
-		//플로이드
-		for(int k=1; k<=V; k++) { //경유지
-			for(int i=1; i<=V; i++) { //출발
-				for(int j=1; j<=V; j++) { //도착
-					if(dist[i][j] > dist[i][k] + dist[k][j]) { //경유지를 거치는 비용이 더 작으면
-						dist[i][j] = dist[i][k] + dist[k][j]; //갱신
+
+		findLoad();
+		result = result == INF ? -1 : result;
+		System.out.println(result);
+	}// main
+
+	private static void findLoad() {
+
+		// 우선 각 사이클이 아닌 각 도착지 까지 가는 거리 구하기
+		// 경유
+		for (int i = 1; i <= V; i++) {
+			// 출발
+			for (int j = 1; j <= V; j++) {
+				// 도착
+				for (int k = 1; k <= V; k++) {
+					// 기존 값보다 경유하고 왔을 경우가 짧으면 갱신
+					if (town[j][k] > town[j][i] + town[i][k]) {
+						town[j][k] = town[j][i] + town[i][k];
 					}
 				}
 			}
 		}
-		
-		int ans = INF; //정답
-		
-		//(참고) 사이클이 돈다는 건 
-		for(int i=1; i<=V; i++) {
-			for(int j=1; j<=V; j++) {
-				if(i == j) continue; //자기 자신을 제외한 두 정점이 
-				
-				//서로에게 가는 경로가 있다는 것
-				if(dist[i][j] != INF && dist[j][i] != INF) {
-					ans = Math.min(ans, dist[i][j]+dist[j][i]);
-				}
-			}
-		}
-		
-		if(ans >= INF) ans = -1;
-		
-		System.out.println(ans);
-	}
 
+		// 사이클 중 최솟값 구하기
+		for (int i = 1; i <= V; i++) {
+			result = Math.min(result, town[i][i]);
+		}
+	}// findLoad
 }
