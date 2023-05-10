b111a279fe675211d897eddd65f8de610ab8ae07
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2023-05-11 08:22:50 +0900

    [D2] Title: 지그재그 숫자, Time: 95 ms, Memory: 42,440 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for tc in range(1, T+1):
+  n = int(input())
+
+  answer = 0
+  for i in range(1, n+1):
+    if(i%2==0):
+      answer -= i
+    else:
+      answer += i
+
+  print(f'#{tc} {answer}')
